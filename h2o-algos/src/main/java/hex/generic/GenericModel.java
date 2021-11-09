493daea8c92ab999280f4ae78057fbd18dcb8087
Marek Novotný <marek.novotny@h2o.ai>
2021-11-10 00:09:52 +0100

    Remove model from metrics

--- h2o-algos/src/main/java/hex/generic/GenericModel.java
+++ h2o-algos/src/main/java/hex/generic/GenericModel.java
@@ -197,11 +197,6 @@ public class GenericModel extends Model<GenericModel, GenericModelParameters, Ge
         public ModelMetrics makeModelMetrics(Model m, Frame f, Frame adaptedFrame, Frame preds) {
             return null;
         }
-
-        @Override
-        public ModelMetrics makeModelMetricsWithoutRuntime(Model m) {
-            return null;
-        }
     }
 
     @Override
